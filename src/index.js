d46af1e3e8524848ee1a993e435e77092fd25165
Fuzi18 <fuzesi.tamas@petrik.hu>
2022-10-12 13:56:06 +0200

    s

---
+++ src/index.js
@@ -0,0 +1,5 @@
+document.addEventListener('DOMContentLoaded', () => {
+  var f;
+  (f = document.getElementById('username')) === null || f === void 0 ? void 0 : f.addEventListener('change', () => {
+  });
+});
