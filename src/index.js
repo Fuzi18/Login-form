0eb1b0e253f3bf192ad0b834ca1468301220e461
Fuzi18 <fuzesi.tamas@petrik.hu>
2022-10-12 14:33:17 +0200

    s

--- src/index.js
+++ src/index.js
@@ -1,5 +1,22 @@
-document.addEventListener('DOMContentLoaded', () => {
-  var f;
-  (f = document.getElementById('username')) === null || f === void 0 ? void 0 : f.addEventListener('change', () => {
-  });
+document.addEventListener("DOMContentLoaded",()=>{
+  console.log("loaded")
+  document.getElementById("username").addEventListener("input",()=>{
+      let username=document.getElementById("username").value;
+      let email=document.getElementById("email").value;
+      let valami = validateEmail(email);
+      if (valami=true) {
+          console.log("a")
+      }else{
+          console.log("b")
+      }
+      if (username) {
+          
+      }
+  })
+
+function validEmail(Email){
+  const re =
+  /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
+  return re.test(Email)
+}
 });
